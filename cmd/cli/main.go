f9987fd9b54a9962af8aed232ea1324931600886
markbang <markbang@talesofai.com>
2026-07-08 12:36:28 +0000

    revert: remove topics/sample endpoints (not needed per design)

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -45,8 +45,6 @@ func main() {
 	rootCmd.AddCommand(cmdExport())
 	rootCmd.AddCommand(cmdLint())
 	rootCmd.AddCommand(cmdDomains())
-	rootCmd.AddCommand(cmdTopics())
-	rootCmd.AddCommand(cmdSample())
 	rootCmd.AddCommand(cmdMigrate())
 	rootCmd.AddCommand(cmdAuth())
 	rootCmd.AddCommand(cmdUpdate())
